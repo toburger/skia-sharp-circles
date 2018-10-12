d5d8a1892f8e011ed3bd7839d304a292c27b8216
Tobias Burger <tobias_burger@hotmail.com>
2018-10-12 23:54:59 +0200

    move image processing logic to separate file.

---
+++ ImageProcessing.fs
@@ -0,0 +1,86 @@
+﻿module ImageProcessing
+
+open System
+open System.Threading.Tasks
+open SkiaSharp
+
+/// Creates a random 'light' color.
+let rndColor =
+    let rnd = Random()
+    let next () = byte (rnd.Next(127, 256))
+    fun () -> SKColor(next (), next (), next (), 255uy)
+
+/// Calculate distance between two points.
+let distance (mmx: int, mmy: int) (ox: int, oy: int) =
+    Math.Sqrt(float ((mmx-ox)*(mmx-ox)+(mmy-oy)*(mmy-oy)))
+
+/// Blend two colors with alpha together.
+let blend (a: SKColor) (b: SKColor) =
+    let aA, rA, gA, bA = int a.Alpha, int a.Red, int a.Green, int a.Blue
+    let aB, rB, gB, bB = int b.Alpha, int b.Red, int b.Green, int b.Blue
+    let rOut = (rA * aA / 255) + (rB * aB * (255 - aA) / (255 * 255))
+    let gOut = (gA * aA / 255) + (gB * aB * (255 - aA) / (255 * 255))
+    let bOut = (bA * aA / 255) + (bB * aB * (255 - aA) / (255 * 255))
+    let aOut = aA + (aB * (255 - aA) / 255)
+    SKColor(byte rOut, byte gOut, byte bOut, byte aOut)
+
+/// <summary>Recolors a bitmap based on an overlay bitmap.</summary>
+/// <remarks>
+/// The overlay bitmap is encoded as a gray color map (for antialising),
+/// where black is ignored and every grayish/white color gets blended into the
+/// original bitmap.
+/// </remarks>
+/// <param name="getClosesColor">Is used to determine a new color of a pixel.</param>
+/// <param name="overlay">The grayscale bitmap which gets blended.</param>
+/// <param name="getClosesColor">The original bitmap to modify</param>
+let recolorPixels
+        getClosestColor
+        (alpha: byte)
+        (overlay: SKBitmap)
+        (original: SKBitmap): unit =
+    let inline updatePixel x y =
+        let color = original.GetPixel(x, y)
+        let grayColor = overlay.GetPixel(x, y)
+        /// Calculate the grayscale from Color.
+        let grayscale =
+           byte
+            ((int grayColor.Red +
+              int grayColor.Green +
+              int grayColor.Blue) / 3)
+        /// If grayscale not black...
+        if grayscale > 0uy then
+            /// Find the closest color.
+            let (ncolor: SKColor) = getClosestColor (x, y)
+            /// Make it half transparent.
+            let alpha = byte ((int grayscale * int alpha) / 255)
+            /// Apply alpha to the closest color.
+            let ncolor = ncolor.WithAlpha(alpha)
+            original.SetPixel(x, y, blend ncolor color)
+        else ()
+    /// Executes in parallel.
+    Parallel.For(0, original.Width, (fun x ->
+        for y = 0 to original.Height - 1 do
+            updatePixel x y))
+    |> ignore
+
+/// Creates a grayscale color map.
+let getColorMap (width, height) radius ccircles: SKBitmap =
+    let info = SKImageInfo(width, height)
+    use surface = SKSurface.Create(info)
+    let canvas = surface.Canvas
+    canvas.Clear(SKColors.Black)
+    for (x, y), _ in ccircles do
+        let color = SKColors.White
+        use paint =
+            new SKPaint(
+                IsAntialias = true,
+                Color = color,
+                FilterQuality = SKFilterQuality.High
+            )
+        canvas.DrawCircle(
+            SKPoint(float32 x, float32 y),
+            float32 radius,
+            paint
+        )
+    use image = surface.Snapshot()
+    SKBitmap.FromImage(image)
--- Program.fs
+++ Program.fs
@@ -1,7 +1,7 @@
 ﻿open System
-open System.Threading.Tasks
 open SkiaSharp
 open Argu
+open ImageProcessing
 
 type Arguments =
     | Radius of int
@@ -23,86 +23,23 @@ type Arguments =
 /// Used to read input from JSON.
 type Input = { id: string; x: string; y: string }
 
-/// Creates a random 'light' color.
-let rndColor =
-    let rnd = Random()
-    let next () = byte (rnd.Next(127, 256))
-    fun () -> SKColor(next (), next (), next (), 255uy)
+let checkFileEnding ext (path: string) =
+    if IO.Path.GetExtension(path) <> ext then
+        failwithf
+            "Invalid file extension. Expected a %s file."
+            (ext.[1..].ToUpper())
+    else
+        path
 
-/// Calculate distance between two points.
-let distance (mmx: int, mmy: int) (ox: int, oy: int) =
-    Math.Sqrt(float ((mmx-ox)*(mmx-ox)+(mmy-oy)*(mmy-oy)))
+let checkExists path =
+    if not (IO.File.Exists(path)) then
+        failwithf "File not found: %s" path
+    else
+        path
 
-/// Blend two colors with alpha together.
-let blend (a: SKColor) (b: SKColor) =
-    let aA, rA, gA, bA = int a.Alpha, int a.Red, int a.Green, int a.Blue
-    let aB, rB, gB, bB = int b.Alpha, int b.Red, int b.Green, int b.Blue
-    let rOut = (rA * aA / 255) + (rB * aB * (255 - aA) / (255 * 255))
-    let gOut = (gA * aA / 255) + (gB * aB * (255 - aA) / (255 * 255))
-    let bOut = (bA * aA / 255) + (bB * aB * (255 - aA) / (255 * 255))
-    let aOut = aA + (aB * (255 - aA) / 255)
-    SKColor(byte rOut, byte gOut, byte bOut, byte aOut)
-
-/// <summary>Recolors a bitmap based on an overlay bitmap.</summary>
-/// <remarks>
-/// The overlay bitmap is encoded as a gray color map (for antialising),
-/// where black is ignored and every grayish/white color gets blended into the
-/// original bitmap.
-/// </remarks>
-/// <param name="getClosesColor">Is used to determine a new color of a pixel.</param>
-/// <param name="overlay">The grayscale bitmap which gets blended.</param>
-/// <param name="getClosesColor">The original bitmap to modify</param>
-let recolorPixels
-        getClosestColor
-        alpha
-        (overlay: SKBitmap)
-        (original: SKBitmap): unit =
-    let inline updatePixel x y =
-        let color = original.GetPixel(x, y)
-        let grayColor = overlay.GetPixel(x, y)
-        /// Calculate the grayscale from Color.
-        let grayscale =
-           byte
-            ((int grayColor.Red +
-              int grayColor.Green +
-              int grayColor.Blue) / 3)
-        /// If grayscale not black...
-        if grayscale > 0uy then
-            /// Find the closest color.
-            let (ncolor: SKColor) = getClosestColor (x, y)
-            /// Make it half transparent.
-            let alpha = byte ((int grayscale * int alpha) / 255)
-            /// Apply alpha to the closest color.
-            let ncolor = ncolor.WithAlpha(alpha)
-            original.SetPixel(x, y, blend ncolor color)
-        else ()
-    /// Executes in parallel.
-    Parallel.For(0, original.Width, (fun x ->
-        for y = 0 to original.Height - 1 do
-            updatePixel x y))
-    |> ignore
-
-/// Creates a grayscale color map.
-let getColorMap (width, height) radius ccircles: SKBitmap =
-    let info = SKImageInfo(width, height)
-    use surface = SKSurface.Create(info)
-    let canvas = surface.Canvas
-    canvas.Clear(SKColors.Black)
-    for (x, y), _ in ccircles do
-        let color = SKColors.White
-        use paint =
-            new SKPaint(
-                IsAntialias = true,
-                Color = color,
-                FilterQuality = SKFilterQuality.High
-            )
-        canvas.DrawCircle(
-            SKPoint(float32 x, float32 y),
-            float32 radius,
-            paint
-        )
-    use image = surface.Snapshot()
-    SKBitmap.FromImage(image)
+let readJson input =
+    IO.File.ReadAllText(input)
+    |> Newtonsoft.Json.JsonConvert.DeserializeObject<Input array>
 
 [<EntryPoint>]
 let main args =
@@ -131,20 +68,6 @@ let main args =
         result.TryGetResult <@ Alpha @>
         |> Option.defaultValue 127uy
 
-    let checkFileEnding ext (path: string) =
-        if IO.Path.GetExtension(path) <> ext then
-            failwithf
-                "Invalid file extension. Expected a %s file."
-                (ext.[1..].ToUpper())
-        else
-            path
-
-    let checkExists path =
-        if not (IO.File.Exists(path)) then
-            failwithf "File not found: %s" path
-        else
-            path
-
     let originalImage =
         result.TryGetResult <@ OriginalImage @>
         |> Option.defaultValue "./burgstall.jpg"
@@ -166,8 +89,7 @@ let main args =
     /// Array of circles with a random color.
     /// Imports the circles from a JSON file.
     let ccircles =
-        IO.File.ReadAllText(input)
-        |> Newtonsoft.Json.JsonConvert.DeserializeObject<Input array>
+        readJson input
         |> Array.map (fun i ->
             let color = rndColor ()
             let x = int i.x * scale
